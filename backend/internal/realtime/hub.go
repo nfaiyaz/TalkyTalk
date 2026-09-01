071279d67e2dfed3bf7c90b49124028bada35a03
Faiyaz Noor <noorfaiyaz317@gmail.com>
2026-09-01 22:16:51 +0600

    Created realtime Websocket hub

---
+++ backend/internal/realtime/hub.go
@@ -0,0 +1,68 @@
+package realtime
+
+import (
+	"encoding/json"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Event struct {
+	Type string `json:"type"`
+	Data any    `json:"data"`
+}
+
+type Hub struct {
+	mu      sync.RWMutex
+	writeMu sync.Mutex
+	clients map[int64]map[*websocket.Conn]struct{}
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients: make(map[int64]map[*websocket.Conn]struct{}),
+	}
+}
+
+func (h *Hub) Add(userID int64, conn *websocket.Conn) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if h.clients[userID] == nil {
+		h.clients[userID] = make(map[*websocket.Conn]struct{})
+	}
+
+	h.clients[userID][conn] = struct{}{}
+}
+
+func (h *Hub) Remove(userID int64, conn *websocket.Conn) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if h.clients[userID] != nil {
+		delete(h.clients[userID], conn)
+
+		if len(h.clients[userID]) == 0 {
+			delete(h.clients, userID)
+		}
+	}
+}
+
+func (h *Hub) SendToUsers(userIDs []int64, event Event) {
+	payload, err := json.Marshal(event)
+	if err != nil {
+		return
+	}
+
+	h.writeMu.Lock()
+	defer h.writeMu.Unlock()
+
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+
+	for _, userID := range userIDs {
+		for conn := range h.clients[userID] {
+			_ = conn.WriteMessage(websocket.TextMessage, payload)
+		}
+	}
+}
